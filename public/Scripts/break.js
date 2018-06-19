ce2bd89dd3caf90a9d629bacaa024a46a0691009
DESKTOP-SP83B60\willi <williamwgilmore@gmail.com>
2018-06-19 17:08:47 -0500

    Adding comments to seperate sections of code

--- public/Scripts/break.js
+++ public/Scripts/break.js
@@ -1,13 +1,21 @@
+//Drawable area
 let canvas = document.getElementById("canvasElement");
 let context = canvas.getContext("2d");
+
+//-------------------------Ball section----------------
+//Ball starting position
 let x = canvas.width/2;
 let y = canvas.height-30;
+//Ball movement speed and direction
 let dx = 2;
 let dy = -2;
+//Ball size
 let ballRadius = 10;
+//Collision size
 let collisionRadius = 8
+//Colors
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'gray', 'black', 'white', 'cyan', 'pink'];
-let ballColor = 0;
+let ballColor = 0; //Starting with red
 
 function drawBall() {
     context.beginPath();
@@ -33,6 +41,10 @@ function cycleColor() {
 	ballColor = ballColor == 11 ? 0 : ballColor + 1;
 }
 
+//-------------------------Paddle section----------------
+
+
+//-------------------------Create section----------------
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
@@ -40,5 +52,5 @@ function draw() {
     x += dx;
     y += dy;
 }
-
+//Runs every 10 miliseconds
 setInterval(draw, 10);
